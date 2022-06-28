608744cd4c81f949ca79ee6a216aafab2e287efc
domiguo818 <1193224158@qq.com>
2022-06-28 20:06:24 +0800

    提交更新

---
+++ vuecli-demo/src/翻转字符串.vue
@@ -0,0 +1,31 @@
+<template>
+  <div >
+    <h1>{{msg}}</h1>
+    <button @click="toggle">翻转</button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+ data(){
+  return{
+    msg:"hello world!"
+  }
+ },
+ methods: {
+  toggle(){
+    let arr = this.msg.split('')
+    console.log(arr);
+    // console.log(arr.reverse());
+    this.msg = arr.reverse().join('');
+  }
+ }
+ 
+}
+</script>
+
+<style>
+
+</style>
